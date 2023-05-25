1804b050b8f24630af62678b7a6f10d4eb798354
ermirasekiraqa <ermiraa.sekiraqa@gmail.com>
2023-05-25 12:16:31 +0200

    Each product is taken straight from database, and then displayed on shop.html.

--- js/shop-functions.js
+++ js/shop-functions.js
@@ -1,146 +1,148 @@
 // Create products cards
 function outputProduct(item) {
-    let productDiv = document.createElement("div");
-    productDiv.className = "product";
-    productDiv.dataset.productId = item.product.id;
-    productDiv.appendChild(createProductImg(item));
-    productDiv.appendChild(createProductDescription(item));
-    return productDiv;
+  let productDiv = document.createElement("div");
+  productDiv.className = "product";
+  productDiv.dataset.productId = item.id;
+  productDiv.appendChild(createProductImg(item));
+  productDiv.appendChild(createProductDescription(item));
+  return productDiv;
 }
 
 function createProductImg(item) {
-    let productAnchor = document.createElement("a");
-    productAnchor.href = "product.html";
-    productAnchor.onclick = setProductToLoadInSessionStorage;
-    let productImg = document.createElement("img");
-    productImg.src = item.product.filename;
-    productAnchor.appendChild(productImg);
-    return productAnchor;
+  let productAnchor = document.createElement("a");
+  productAnchor.href = "product.html";
+  // productAnchor.onclick = setProductToLoadInSessionStorage;
+  let productImg = document.createElement("img");
+  productImg.src = item.image;
+  console.log(productImg.src);
+  productAnchor.appendChild(productImg);
+  return productAnchor;
 }
 
 function createProductDescription(item) {
-    let productDescription = document.createElement("div");
-    productDescription.className = "product-description";
-    productDescription.appendChild(createProductBrand(item));
-    productDescription.appendChild(createProductName(item));
-    productDescription.appendChild(createProductRatings(item));
-    productDescription.appendChild(createProductPrice(item));
-    productDescription.appendChild(createShoppingCartIcon(item));
-    return productDescription;
+  let productDescription = document.createElement("div");
+  productDescription.className = "product-description";
+  productDescription.appendChild(createProductBrand(item));
+  productDescription.appendChild(createProductName(item));
+  productDescription.appendChild(createProductPrice(item));
+  productDescription.appendChild(createShoppingCartIcon(item));
+  return productDescription;
 }
 
 function createProductBrand(item) {
-    let brand = document.createElement("span");
-    brand.innerText = item.product.brand;
-    return brand;
+  let brand = document.createElement("span");
+  brand.innerText = item.brand;
+  return brand;
 }
 
 function createProductName(item) {
-    let productName = document.createElement("h5");
-    productName.innerText = item.product.productname;
-    return productName;
-}
-
-function createProductRatings(item) {
-    let productRatings = document.createElement("div");
-    productRatings.className = "ratings";
-    for (let i = 0; i < item.product.rates; i++) {
-        let star = document.createElement("i");
-        star.className = "fa fa-star";
-        productRatings.appendChild(star);
-    }
-    return productRatings;
+  let productName = document.createElement("h5");
+  productName.innerText = item.productname;
+  return productName;
 }
 
 function createProductPrice(item) {
-    let productPrice = document.createElement("h4");
-    productPrice.innerText = "$" + item.product.price;
-    return productPrice;
+  let productPrice = document.createElement("h4");
+  productPrice.innerText = "$" + item.price;
+  return productPrice;
 }
 
 function createShoppingCartIcon(item) {
-    let cartAnchor = document.createElement("a");
-    cartAnchor.href = "product.html";
-    cartAnchor.onclick = setProductToLoadInSessionStorageFromIcon;
-    let cartIcon = document.createElement("i");
-    cartIcon.className = "fa fa-shopping-cart cart";
-    cartAnchor.appendChild(cartIcon);
-    return cartAnchor;
+  let cartAnchor = document.createElement("a");
+  cartAnchor.href = "product.html";
+  // cartAnchor.onclick = setProductToLoadInSessionStorageFromIcon;
+  let cartIcon = document.createElement("i");
+  cartIcon.className = "fa fa-shopping-cart cart";
+  cartAnchor.appendChild(cartIcon);
+  return cartAnchor;
 }
 
-// Load single product page
-function setProductToLoadInSessionStorage(clickEvent) {
-    let productId = parseInt(clickEvent.currentTarget.parentElement.dataset.productId);
-    sessionStorage.setItem("loadedProductPage", productId);
-}
-
-// Load single product page from shopping cart icon
-function setProductToLoadInSessionStorageFromIcon(clickEvent) {
-    let productId = parseInt(clickEvent.currentTarget.parentElement.parentElement.dataset.productId);
-    sessionStorage.setItem("loadedProductPage", productId);
+// Display products
+function displayProducts(products) {
+  productSection.innerHTML = "";
+  for (let i = (currentPage - 1) * (rows * 3); i < currentPage * rows * 3 && i < products.length; i++) {
+    productSection.appendChild(outputProduct(products[i]));
+  }
 }
 
 let productSection = document.getElementById("product-container");
-
-// Pagination
 let rows = 3;
-let currentPage = 1;  // Current page
-let pageSize = Math.ceil(products.length / (rows * 3));
+let currentPage = 1;  // Current page 
 
-function displayProducts(products) {
-    productSection.innerHTML = "";
-    for (let i = (currentPage - 1) * (rows * 3); i < currentPage * rows * 3 && i < products.length; i++) {
-        productSection.appendChild(outputProduct(products[i]));
-    }
-}
+// Display pagination buttons
+function displayPaginationButtons(pageCount) {
+  let paginationSection = document.getElementsByClassName("pages").item(0);
+  if (pageCount > 1) {
+    let prevButton = document.createElement("button");
+    prevButton.onclick = prevPage;
+    let leftArrowIcon = document.createElement("i");
+    leftArrowIcon.className = "fa fa-long-arrow-left";
+    prevButton.appendChild(leftArrowIcon);
+    paginationSection.appendChild(prevButton);
+  }
 
-function displayPaginationButtons(num) {
-    let paginationSection = document.getElementsByClassName("pages").item(0);
-    if (num > 1) {
-        let prevButton = document.createElement("button");
-        prevButton.onclick = prevPage;
-        let leftArrowIcon = document.createElement("i");
-        leftArrowIcon.className = "fa fa-long-arrow-left";
-        prevButton.appendChild(leftArrowIcon);
-        paginationSection.appendChild(prevButton);
-    }
+  for (let i = 0; i < pageCount; i++) {
+    let numButton = document.createElement("button");
+    numButton.onclick = loadPage;
+    numButton.innerText = i + 1;
+    paginationSection.appendChild(numButton);
+  }
 
-    for (let i = 0; i < num; i++) {
-        let numButton = document.createElement("button");
-        numButton.onclick = loadPage;
-        numButton.innerText = i + 1;
-        paginationSection.appendChild(numButton);
-    }
-
-    if (num > 1) {
-        let nextButton = document.createElement("button");
-        nextButton.onclick = nextPage;
-        let rightArrowIcon = document.createElement("i");
-        rightArrowIcon.className = "fa fa-long-arrow-right";
-        nextButton.appendChild(rightArrowIcon);
-        paginationSection.appendChild(nextButton);
-    }
+  if (pageCount > 1) {
+    let nextButton = document.createElement("button");
+    nextButton.onclick = nextPage;
+    let rightArrowIcon = document.createElement("i");
+    rightArrowIcon.className = "fa fa-long-arrow-right";
+    nextButton.appendChild(rightArrowIcon);
+    paginationSection.appendChild(nextButton);
+  }
 }
 
 function nextPage() {
-    if (currentPage === pageSize)
-        return;
-    currentPage += 1;
-    displayProducts(products);
+  if (currentPage === pageSize) return;
+  currentPage += 1;
+  fetchProducts();
 }
 
 function prevPage() {
-    if (currentPage === 1)
-        return;
-    currentPage -= 1;
-    displayProducts(products);
+  if (currentPage === 1) return;
+  currentPage -= 1;
+  fetchProducts();
 }
 
 function loadPage(clickEvent) {
-    let pageNumber = parseInt(clickEvent.target.innerText);
-    currentPage = pageNumber;
-    displayProducts(products);
+  let pageNumber = parseInt(clickEvent.target.innerText);
+  currentPage = pageNumber;
+  fetchProducts();
 }
 
-displayProducts(products);
-displayPaginationButtons(pageSize);
+// Fetch product data from PHP using AJAX
+function fetchProducts() {
+  // Make an AJAX request to products.php
+  fetch("products.php")
+    .then((response) => response.json())
+    .then((data) => {
+      displayProducts(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching product data:", error);
+    });
+}
+
+function getPageCountAndDisplayPaginationButtons() {
+  // Make an AJAX request to products.php
+  fetch("products.php")
+    .then((response) => response.json())
+    .then((data) => {
+      // Pagination
+      pageSize = Math.ceil(data.length / (rows * 3));
+      displayPaginationButtons(pageSize);
+    })
+    .catch((error) => {
+      console.error("Error fetching product data:", error);
+    });
+}
+
+// Load initial page
+fetchProducts();
+getPageCountAndDisplayPaginationButtons();
