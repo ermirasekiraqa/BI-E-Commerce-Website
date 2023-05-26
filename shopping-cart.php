1d251614c43f224d2c3b1d7b8fa2e70a129ee750
ermirasekiraqa <ermiraa.sekiraqa@gmail.com>
2023-05-26 16:36:07 +0200

    Remove items from cart after order is made.

--- shopping-cart.php
+++ shopping-cart.php
@@ -20,7 +20,7 @@ echo '<!DOCTYPE html>
       <link rel="stylesheet" href="css\shopping-cart-style.css" />
     
       <script defer src="js\header-functions.js"></script>
-      <!-- <script defer src="js\shopping-cart.js"></script> -->
+      <script defer src="js\removeProductFromCart.js"></script>
     
       <title>Cart</title>
     
@@ -42,20 +42,7 @@ echo '<!DOCTYPE html>
         </nav>
       </header>
       <main>
-        <div class="shopping-cart-and-button">
-        <div id="shopping-cart">
-          <table class="cart-table" cellspacing="0" cellpadding="10">
-          <thead>
-          <tr>
-          <th>ID</th>
-          <th>Image</th>
-          <th>Name</th>
-          <th>Price</th>
-          <th>Quantity</th>
-          <th></th>
-          </tr>
-          </thead>
-          <tbody>';
+        <div class="shopping-cart-and-button">';
 
 // Retrieve cart items from the database or session storage
 $cartItems = [];
@@ -112,84 +99,94 @@ if (isset($_SESSION['email'])) {
   // Access the session storage data and assign it to $cartItems
   if (isset($_SESSION['cart_items'])) {
     $cartItems = $_SESSION['cart_items'];
-  } else {
-    $cartItems = []; // Empty array for guest user
-    echo 'Cart is empty';
   }
 }
 
-// Display the items from $cartItems
-// Connect to the database
-$conn = new mysqli($servername, $username, $password, $dbname);
+if (!empty($cartItems)) {
+  echo '<div id="shopping-cart">
+    <table class="cart-table" cellspacing="0" cellpadding="10">
+    <thead>
+    <tr>
+    <th>ID</th>
+    <th>Image</th>
+    <th>Name</th>
+    <th>Price</th>
+    <th>Quantity</th>
+    <th></th>
+    </tr>
+    </thead>
+    <tbody>';
+  // Display the items from $cartItems
+  // Connect to the database
+  $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check if the connection was successful
-if ($conn->connect_error) {
-  die('Database connection failed: ' . $conn->connect_error);
-}
+  // Check if the connection was successful
+  if ($conn->connect_error) {
+    die('Database connection failed: ' . $conn->connect_error);
+  }
 
-$counter = 1;
-// Fetch product details for each cart item
-foreach ($cartItems as &$item) {
-  $productId = $item['product_id'];
+  $counter = 1;
+  // Fetch product details for each cart item
+  foreach ($cartItems as &$item) {
+    $productId = $item['product_id'];
 
-  // Prepare the SQL statement to fetch product details
-  $sql = "SELECT name, price, image_url FROM product WHERE product_id = ?";
+    // Prepare the SQL statement to fetch product details
+    $sql = "SELECT name, price, image_url FROM product WHERE product_id = ?";
 
-  // Prepare and bind the parameter
-  $stmt = $conn->prepare($sql);
-  $stmt->bind_param('i', $productId);
+    // Prepare and bind the parameter
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('i', $productId);
 
-  // Execute the query
-  $stmt->execute();
+    // Execute the query
+    $stmt->execute();
 
-  // Bind the result variables
-  $stmt->bind_result($name, $price, $image_url);
+    // Bind the result variables
+    $stmt->bind_result($name, $price, $image_url);
 
-  // Fetch the result
-  $stmt->fetch();
+    // Fetch the result
+    $stmt->fetch();
 
-  // Assign the fetched values to the cart item
-  $item['name'] = $name;
-  $item['price'] = $price;
-  $item['image_url'] = $image_url;
+    // Assign the fetched values to the cart item
+    $item['name'] = $name;
+    $item['price'] = $price;
+    $item['image_url'] = $image_url;
+    echo '<tr>';
+    // Display the ID
+    echo '<td>' . $counter . '</td>';
+    $counter = $counter + 1;
 
-  echo '<tr>';
-  // Display the ID
-  echo '<td>' . $counter . '</td>';
-  $counter = $counter + 1;
+    // Display the Image
+    echo '<td><img src="images/' . $item['image_url'] . '" alt="Product Image"></td>';
 
-  // Display the Image
-  echo '<td><img src="images/' . $item['image_url'] . '" alt="Product Image"></td>';
+    // Display the Name
+    echo '<td>' . $item['name'] . '</td>';
 
-  // Display the Name
-  echo '<td>' . $item['name'] . '</td>';
+    // Display the Price
+    echo '<td>$' . $item['price'] . '</td>';
 
-  // Display the Price
-  echo '<td>$' . $item['price'] . '</td>';
+    // Display the Quantity
+    echo '<td>' . $item['quantity'] . '</td>';
 
-  // Display the Quantity
-  echo '<td>' . $item['quantity'] . '</td>';
+    // Display the trash icon
+    echo '<td><i class="fa fa-trash trash-icon"></i></td>';
 
-  // Display the trash icon
-  echo '<td><i class="fa fa-trash"></i></td>';
+    echo '</tr>';
 
-  echo '</tr>';
+    // Close the statement
+    $stmt->close();
+  }
 
-  // Close the statement
-  $stmt->close();
-}
+  // Close the database connection
+  $conn->close();
 
-// Close the database connection
-$conn->close();
+  // Calculate the total price of all items in the cart
+  $totalPrice = 0;
+  foreach ($cartItems as $item) {
+    $totalPrice += ($item['price'] * $item['quantity']);
+  }
 
-// Calculate the total price of all items in the cart
-$totalPrice = 0;
-foreach ($cartItems as $item) {
-  $totalPrice += ($item['price'] * $item['quantity']);
-}
-
-// Display the total price
-echo '<tr>
+  // Display the total price
+  echo '<tr>
   <td></td>
   <td></td>
   <td></td>
@@ -200,53 +197,65 @@ echo '<tr>
   </tbody>
 </table>
 </div>
-  </main>
-  <footer class="footer-with-gray-background">
-    <section class="useful-info">
-        <div class="get-in-touch">
-            <h3>Head Office</h3>
-            <div class="get-in-touch-info">
-                <li>
-                    <i class="fa fa-map-o"></i>
-                    <p>See map for our location!</p>
-                </li>
-                <li>
-                    <i class="fa fa-envelope-o"></i>
-                    <p>eye-wear@gmail.com</p>
-                </li>
-                <li>
-                    <i class="fa fa-phone"></i>
-                    <p>+12 345 6789</p>
-                </li>
-                <li>
-                    <i class="fa fa-clock-o"></i>
-                    <p>Monday to Saturday, 9:00AM to 16.PM</p>
-                </li>
-            </div>
-        </div>
-        <div class="quick-links">
-            <h3>Quick Links</h3>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="about-us.html">About Us</a></li>
-            <li><a href="shop.html">Shop Now</a></li>
-            <li><a href="contact.html">Contact Us</a></li>
-            <li><a href="login.html">Log in</a></li>
-            <li><a href="signup.html">Sign up</a></li>
-        </div>
-        <div class="logo-in-footer">
-            <img src="images/logo.png" alt="Logo">
-        </div>
-    </section>
-    <section class="copyright-social-media">
-        <p>
-            Copyright © 2023 Universiteti i Prishtines "Hasan Prishtina", FSHMN, Shkenca Kompjuterike
-        </p>
-        <div id="social-media-icons">
-            <i class="fa fa-twitter"></i>
-            <i class="fa fa-google-plus"></i>
-            <i class="fa fa-linkedin"></i>
-        </div>
-    </section>
+<div id="checkout-button-div">
+  <form action="checkout.php" method="post">
+  <!-- Add any necessary input fields related to the order details -->
+  <input type="submit" value="Checkout" id="checkout-button">
+  </form>
+</div>';
+} else {
+  $cartItems = []; // Empty array for guest user
+  echo '<p id="cart-is-empty-message">Cart is empty!</p>';
+}
+
+echo '</div>
+</main>
+<footer class="footer-with-gray-background">
+  <section class="useful-info">
+      <div class="get-in-touch">
+          <h3>Head Office</h3>
+          <div class="get-in-touch-info">
+              <li>
+                  <i class="fa fa-map-o"></i>
+                  <p>See map for our location!</p>
+              </li>
+              <li>
+                  <i class="fa fa-envelope-o"></i>
+                  <p>eye-wear@gmail.com</p>
+              </li>
+              <li>
+                  <i class="fa fa-phone"></i>
+                  <p>+12 345 6789</p>
+              </li>
+              <li>
+                  <i class="fa fa-clock-o"></i>
+                  <p>Monday to Saturday, 9:00AM to 16.PM</p>
+              </li>
+          </div>
+      </div>
+      <div class="quick-links">
+          <h3>Quick Links</h3>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about-us.html">About Us</a></li>
+          <li><a href="shop.html">Shop Now</a></li>
+          <li><a href="contact.html">Contact Us</a></li>
+          <li><a href="login.html">Log in</a></li>
+          <li><a href="signup.html">Sign up</a></li>
+      </div>
+      <div class="logo-in-footer">
+          <img src="images/logo.png" alt="Logo">
+      </div>
+  </section>
+  <section class="copyright-social-media">
+      <p>
+          Copyright © 2023 Universiteti i Prishtines "Hasan Prishtina", FSHMN, Shkenca Kompjuterike
+      </p>
+      <div id="social-media-icons">
+          <i class="fa fa-twitter"></i>
+          <i class="fa fa-google-plus"></i>
+          <i class="fa fa-linkedin"></i>
+      </div>
+  </section>
 </footer>
 </body>
 
