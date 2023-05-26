aea510fa4811123b26db02e154fd0fc7e0008e7c
ermirasekiraqa <ermiraa.sekiraqa@gmail.com>
2023-05-26 22:14:11 +0200

    When signing up, it will not be allowed to sign up with an email that a user already has.

--- process-order.php
+++ process-order.php
@@ -97,13 +97,6 @@ foreach ($cartItems as &$item) {
     // Assign the fetched values to the cart item
     $item['price'] = $price;
     $totalPrice += ($price * $item['quantity']);
-
-        //     // // Insert the product_id and order_id into the order_products table
-    //     // $sql = "INSERT INTO order_products (order_id) VALUES (?)";
-    //     // $insertStmt  = $conn->prepare($sql);
-    //     // $insertStmt ->bind_param('i', $order_id);
-    //     // $insertStmt ->execute();
-    //     // $insertStmt ->close();
     
     $stmt->close();
 }
@@ -114,35 +107,20 @@ $status = "Processed";
 $sql = "INSERT INTO orders (first_name, last_name, total_price, address, city, country, payment_method, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param('sssssssd', $firstName, $lastName, $totalPrice, $address, $city, $country, $paymentMethod, $status);
-// $stmt->execute();
-// $order_id = $stmt->insert_id;
-// foreach ($cartItems as $item) { 
-//     $productId = intval($item['product_id']);
-//     $quantity = intval($item['quantity']);
 
-//     echo $productId;
-//     echo $quantity;
-// }
-    // // //     // echo json_encode($cartItems);
-    // //     $productId = $item['product_id'];
-    // //     $Quantity = $item['quantity'];
-    // //     echo $productId;
-    // //     echo $Quantity;
-    //     // // Insert the product_id and order_id into the order_products table
-    //     // $sql = "INSERT INTO order_products (order_id) VALUES (?)";
-    //     // $insertStmt  = $conn->prepare($sql);
-    //     // $insertStmt ->bind_param('i', $order_id);
-    //     // $insertStmt ->execute();
-    //     // $insertStmt ->close();
-    //     // Insert the product_id and order_id into the order_products table
-    // $sql = "INSERT INTO order_products (order_id, product_id, quantity) VALUES (?, ?, ?)";
-    // $insertStmt  = $conn->prepare($sql);
-    // $insertStmt ->bind_param('iii', $order_id, $item['product_id'], $item['quantity']);
-    // $insertStmt ->execute();
-    // $insertStmt ->close();
-    // }
 if ($stmt->execute()) {
-    // echo json_encode($cartItems);
+    // Get the order ID
+    $order_id = $stmt->insert_id;
+    foreach ($cartItems as &$item) { 
+        $productId = intval($item['product_id']);
+        $quantity = intval($item['quantity']);
+        // Insert the product_id and order_id into the order_products table
+        $sql = "INSERT INTO order_products (order_id, product_id, quantity) VALUES (?, ?, ?)";
+        $insertStmt  = $conn->prepare($sql);
+        $insertStmt ->bind_param('iii', $order_id, $productId, $quantity);
+        $insertStmt ->execute();
+        $insertStmt ->close();
+    }
     // Clear user's cart after order
     if (isset($_SESSION['email'])) {
         $sql = "DELETE FROM cart WHERE user_id = ?";
@@ -184,7 +162,6 @@ if ($stmt->execute()) {
           <img src="images\logo.png" alt="Black and light red sunglasses">
           <ul>
             <li><a href="index.html">Home</a></li>
-            <li><a href="explore.html"> Explore</a></li>
             <li><a href="shop.html">Shop</a></li>
             <li><a href="about-us.html">About us</a></li>
             <li><a href="contact.html">Contact</a></li>
--- signup.php
+++ signup.php
@@ -26,14 +26,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $country = $_POST['country'];
 
     // Insert user data into the Users table
-    $sql = "INSERT INTO Users (name, last_name, email, password, phone_number, address, city, country) 
-            VALUES ('$name', '$last_name', '$email', '$password', '$phone_number', '$address', '$city', '$country')";
-    $result = mysqli_query($conn, $sql);
+    try {
+        $sql = "INSERT INTO Users (name, last_name, email, password, phone_number, address, city, country) 
+                VALUES ('$name', '$last_name', '$email', '$password', '$phone_number', '$address', '$city', '$country')";
+        $result = mysqli_query($conn, $sql);
 
-    if ($result) {
-        // User registration successful, redirect to a success page or perform other actions
-        // ...
-                // Redirect the user to a success page or perform other actions
+        if ($result) {
+            // User registration successful, redirect to a success page or perform other actions
+            // ...
             echo '<!DOCTYPE html>
             <html lang="en">
             
@@ -60,58 +60,43 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </body>
             
             </html>';
-    } else {
+        }
+    } catch (mysqli_sql_exception $e) {
         // Check if the error is due to a duplicate email entry
-        $error = mysqli_error($conn);
+        $error = $e->getMessage();
         if (strpos($error, "Duplicate entry") !== false && strpos($error, "email") !== false) {
             // Duplicate entry error (unique constraint violation)
             // Display an error message to the user
-            echo "The email is already in use. Please choose another email.";
+            echo '<!DOCTYPE html>
+            <html lang="en">
+            
+            <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="stylesheet" href="css\normalize.css" />
+            <link rel="stylesheet" href="css\signup.css" />
+            
+            <title>Sign up</title>
+            
+            <link rel="icon" type="image/x-icon" href="images\favicon.png">
+            </head>
+            
+            <body>
+            <main>
+                <div class="registration-successful">
+                    <p>The email is already in use. Please choose another email.</p>
+                    <p id="login-link">Click <a href="signup.html">here</a> to try again.</p>
+                </div>
+            </main>
+            </body>
+            
+            </html>';
         } else {
             // Other error occurred
             echo "Error: " . $error;
         }
     }
-//     if (mysqli_query($conn, $sql)) {
-//         // Redirect the user to a success page or perform other actions
-//             echo '<!DOCTYPE html>
-//             <html lang="en">
-            
-//             <head>
-//             <meta charset="UTF-8" />
-//             <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-//             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//             <link rel="stylesheet" href="css\normalize.css" />
-//             <link rel="stylesheet" href="css\signup.css" />
-            
-//             <title>Sign up</title>
-            
-//             <link rel="icon" type="image/x-icon" href="images\favicon.png">
-//             </head>
-            
-//             <body>
-//             <main>
-//                 <div class="registration-successful">
-//                     <p>Registration successful!</p>
-//                     <p id="login-link">Click <a href="login.html">here</a> to login.</p>
-//                     <p id="login-link">Click <a href="index.html">here</a> to go back to the main page.</p>
-//                 </div>
-//             </main>
-//             </body>
-            
-//             </html>';
-//     } else {
-//         if ($stmt->errno === 1062) {
-//             // Duplicate entry error (unique constraint violation)
-//             // Display an error message to the user
-//             echo "The email is already in use. Please choose another email.";
-//         } else {
-//             // Other error occurred
-//             echo "Error: " . $stmt->error;
-//         }
-//     }
 }
-
-// Close the database connection
-mysqli_close($conn);
 ?>
+
